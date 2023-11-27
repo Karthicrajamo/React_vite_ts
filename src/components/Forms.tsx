bd95cc1f1a1366b97e6404c7a1ab32b7237e1baa
Karthic Raja <karthicraja@neulancers.com>
2023-11-27 14:32:18 +0530

    validation without zod module

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,47 +1,52 @@
 import React, { FormEvent, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface Props {
+	name: string;
+	age: number;
+}
 
 const forms = () => {
-	const nameRef = useRef<HTMLInputElement>(null);
-	const ageRef = useRef<HTMLInputElement>(null);
-	const [person, setPerson] = useState({
-		name: "",
-		age: "",
-	});
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Props>();
+	// console.log(register("name"));
+	const onClick = handleSubmit((data) => console.log(data));
 
-	const handle = (event: FormEvent) => {
-		event.preventDefault();
-
-		console.log(person);
-	};
 	return (
-		<form onSubmit={handle}>
+		<form onSubmit={onClick}>
 			<div className="mb-3">
 				<label htmlFor="name" className="form-lable">
 					Name
 				</label>
 				<input
-					onChange={(event) =>
-						setPerson({ ...person, name: event.target.value })
-					}
-					value={person.name}
+					{...register("name", { required: true, minLength: 3 })}
 					id="name"
 					type="text"
 					className="form-control"
 				/>
+				{errors.name?.type === "required" && (
+					<p className="text-danger">Name field is required</p>
+				)}
+				{errors.name?.type === "minLength" && (
+					<p className="text-danger">The name must be atleast 3 char</p>
+				)}
 			</div>
 			<div className="mb-3">
 				<label htmlFor="age" className="form-label">
 					Age
 				</label>
 				<input
-					onChange={(event) =>
-						setPerson({ ...person, age: parseInt(event.target.value) })
-					}
-					value={person.age}
+					{...register("age", { required: true })}
 					id="age"
 					type="number"
 					className="form-control"
 				/>
+				{errors.age?.type === "required" && (
+					<p className="text-danger">The name field required</p>
+				)}
 			</div>
 			<button className="btn btn-primary">Submit</button>
 		</form>
