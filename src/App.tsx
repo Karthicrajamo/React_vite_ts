bc6aecb178a39d6fc858426a5ec80fe58740503c
Karthic Raja <karthicraja@neulancers.com>
2023-11-29 15:35:04 +0530

    Accessing Data from Django APIs to use in react // And also uses async and await in axios

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,47 @@ import ExpenseList from "./ExpenseTracker/components/ExpenseList";
 import ExpenseFilter from "./ExpenseTracker/components/ExpenseFilter";
 import ExpenseForm from "./ExpenseTracker/components/ExpenseForm";
 import ProductList from "./ProductList";
+import axios, { AxiosError } from "axios";
+
+interface User {
+	id: number;
+	title: string;
+}
 
 function App() {
-	const [category, setCategory] = useState("");
+	const [users, setUsers] = useState<User[]>([]);
+	const [error, setError] = useState("");
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get("http://127.0.0.1:8000/products/");
+				setUsers(response.data.results);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+				setError((error as AxiosError).message);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	// useEffect(() => {
+	// 	axios
+	// 		.get<User[]>("http://127.0.0.1:8000/products/")
+	// 		// .then((res) => console.log(res.data.results));
+	// 		.then((res) => setUsers(res.data.results));
+	// 	// .then((res) => setUsers(res.data));
+	// }, []);
 
 	return (
-		<div>
-			<select
-				className="form-select"
-				onChange={(event) => {
-					setCategory(event?.target.value);
-				}}
-			>
-				<option></option>
-				<option value="Clothing">Clothing</option>
-				<option value="Household">Household</option>
-			</select>
-			<ProductList category={category}></ProductList>
-		</div>
+		<>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>{user.title}</li>
+				))}
+			</ul>
+		</>
 	);
 }
 
