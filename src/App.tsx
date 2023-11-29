1b6b70824bf2475bc036ebbc7c4729a63453f43e
Karthic Raja <karthicraja@neulancers.com>
2023-11-29 13:43:12 +0530

    Intro of useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ExpenseList from "./ExpenseTracker/components/ExpenseList";
 import ExpenseFilter from "./ExpenseTracker/components/ExpenseFilter";
 import ExpenseForm from "./ExpenseTracker/components/ExpenseForm";
+import ProductList from "./ProductList";
 
 function App() {
-	const [expense, setExpense] = useState([
-		{
-			id: 1,
-			description: "des 1",
-			amount: 59,
-			category: "Utility",
-		},
-		{
-			id: 2,
-			description: "des 2",
-			amount: 59,
-			category: "Utility",
-		},
-		{
-			id: 3,
-			description: "des 3",
-			amount: 59,
-			category: "Utility",
-		},
-		{
-			id: 4,
-			description: "des 4",
-			amount: 59,
-			category: "Utility",
-		},
-	]);
-
 	const [category, setCategory] = useState("");
-	const validateExpense = category
-		? expense.filter((e) => e.category == category)
-		: expense;
+
 	return (
 		<div>
-			<div className="mb-5">
-				<ExpenseForm
-					onSubmit={(data) =>
-						setExpense([...expense, { ...data, id: expense.length + 1 }])
-					}
-				></ExpenseForm>
-			</div>
-			<div className="mb-3">
-				<ExpenseFilter onSelect={setCategory}></ExpenseFilter>
-			</div>
-			<ExpenseList
-				expense={validateExpense}
-				onDelete={(id) => setExpense(expense.filter((e) => e.id !== id))}
-			></ExpenseList>
+			<select
+				className="form-select"
+				onChange={(event) => {
+					setCategory(event?.target.value);
+				}}
+			>
+				<option></option>
+				<option value="Clothing">Clothing</option>
+				<option value="Household">Household</option>
+			</select>
+			<ProductList category={category}></ProductList>
 		</div>
 	);
 }
---
+++ src/ProductList.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+const ProductList = ({ category }: { category: string }) => {
+	const [product, setProduct] = useState<string[]>([]);
+	useEffect(() => {
+		console.log("Fetching products", category);
+		setProduct(["Clothing", "Household"]);
+	}, [category]);
+	return <div>ProductList</div>;
+};
+
+export default ProductList;
